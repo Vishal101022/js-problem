f6ffc73166f13613a23cf322e4addbfcbc00a280
Vishal101022 <raivishal939@gmail.com>
2023-10-03 22:08:48 +0530

    curry implementation for multi-argument functions

---
+++ implement_curry.js
@@ -0,0 +1,24 @@
+// write curry function
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+}
+
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+};
+
+const curriedJoin = curry(join);
+
+console.log(curriedJoin(1)(2, 3))
+
+// curriedJoin(1)(2, 3); // '1_2_3'
+
+// curriedJoin(1, 2)(3); // '1_2_3'
