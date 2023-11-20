1871e318ae18cfa1f030f49c9bb8d1c00351b57c
Vishal Rai <raivishal939@gmail.com>
2023-11-20 22:33:55 +0530

    used array method

--- reverse_string.js
+++ reverse_string.js
@@ -1,14 +1,16 @@
 // brute force  approach
 function reverseString(str) {
     // Your code here
-    let temp = "";  // temp varible to store reverse string
+    /*let temp = "";  // temp varible to store reverse string
     // for loop starting from backward
     for(let i=str.length-1; i >= 0; i--){ 
          // taking each char from last and store in temp
          temp += str[i];
     }
     // return temp 
-    return temp;
+    return temp;*/
+    // used array method split | reverse | join
+    return str.split('').reverse().join('');
   }
   
   // Test cases
