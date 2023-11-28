0854321e3f87da4f716b2a417383deb0e6d8f11c
Vishal Rai <raivishal939@gmail.com>
2023-11-28 23:37:32 +0530

    :bulb: add comment

--- large_element_in_array.js
+++ large_element_in_array.js
@@ -1,4 +1,4 @@
-// brute force approach O(n^2)
+// linear search approach O(n) time complexity
 function findLargestElement(arr) {
     // Initialize a variable to store the largest element
     let largestElement = arr[0]; 
@@ -7,7 +7,7 @@ function findLargestElement(arr) {
     for (let i = 1; i < arr.length; i++) {
         // Compare each element with the current largestElement
         // Update largestElement if a larger element is found
-        if(arr[i] > arr[i+1]){
+        if(arr[i] > largestElement) {
             largestElement = arr[i];
         }
         
@@ -20,3 +20,9 @@ function findLargestElement(arr) {
 const array = [5, 2, 8, 9, 1,98,12,109,45,67,89];
 const result = findLargestElement(array);
 console.log("The largest element is:", result);
+
+// i was thinking for better approach 
+  // - first sort the array in ascending order
+  // - then return the last element
+
+// but the time complexity is O(nlogn) which is not good 
