c0f8663f45cf61c2a97a3bbc4e55f1acd742bbb4
Vishal Rai <raivishal939@gmail.com>
2023-12-01 23:45:40 +0530

    O(n) solution

--- check_square_element_of_another_array.js
+++ check_square_element_of_another_array.js
@@ -1,23 +1,38 @@
 function areSquares(arr1, arr2) {
-    // Check if both arrays have the same length
-    if (arr1.length !== arr2.length) {
-        return false;
+    // Initialize two maps to store the count of each element in the respective arrays
+    let map1 = {};
+    let map2 = {};
+
+    // Populate map1 with counts of elements in arr1
+    for (item of arr1) {
+        map1[item] = (map1[item] || 0) + 1;
     }
 
-    // Check if each element in arr2 is the square of the corresponding element in arr1
-    for (let i = 0; i < arr1.length; i++) {
-        if (Math.pow(arr1[i], 2) !== arr2[i]) {
+    // Populate map2 with counts of elements in arr2
+    for (item of arr2) {
+        map2[item] = (map2[item] || 0) + 1;
+    }
+
+    // Check if the squared keys in map1 exist in map2 with matching counts
+    for (let key in map1) {
+        // If the squared key doesn't exist in map2, arrays are not squares
+        if (!map2[key * key]) {
+            return false;
+        }
+        // If the counts of the original key and its square are not the same, arrays are not squares
+        if (map1[key] !== map2[key * key]) {
             return false;
         }
     }
 
-    // If all elements satisfy the condition, return true
+    
     return true;
 }
 
 // Example usage
-let array1 = [2, 3, 4];
-let array2 = [4, 9, 16];
+let array1 = [2, 3, 4, 5];
+let array2 = [9, 4, 25, 16];
+
 
 let result = areSquares(array1, array2);
-console.log(result); 
+console.log(result);
