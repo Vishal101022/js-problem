600b1b8e1d6983dd65b325e19353d50e5113d3c4
Vishal101022 <raivishal939@gmail.com>
2024-01-06 22:46:19 +0530

    sort array in accending order

---
+++ ASC_sort.js
@@ -0,0 +1,22 @@
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+let numbers = [4, 2, 8, 1, 6];
+let sortedNumbers = bubbleSort(numbers.slice());
+console.log("Sorted using Bubble Sort:", sortedNumbers);
